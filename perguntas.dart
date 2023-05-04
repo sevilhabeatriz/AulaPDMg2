fa64a288306c6d1be8ae72b3f195a4e71a10b259
sevilhabeatriz <131026612+sevilhabeatriz@users.noreply.github.com>
2023-05-04 11:54:18 -0300

    Update perguntas.dart

--- perguntas.dart
+++ perguntas.dart
@@ -1,2 +1,20 @@
 import 'package:flutter/material.dart';
 
+class Perguntas extends StatelessWidget(
+  String texto;
+  
+  Pergutas(this.texto);
+  
+  @override
+  Widget build (BuildContext context){
+    return Container{
+      width: double infinity
+      margin:EdgeInstets.all(20),
+     child Text( 
+     texto,
+     style: TextAlign.center,),//text
+     ); //Container
+    } 
+    
+ 
+  }
