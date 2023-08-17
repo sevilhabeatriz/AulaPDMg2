00a8d88d45d1bec267ef5c58ecddac237f89251b
sevilhabeatriz <131026612+sevilhabeatriz@users.noreply.github.com>
2023-08-17 10:13:40 -0300

    Add files via upload

---
+++ categoria_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+  width: 100,
+  height: 100,
+  color: Colors.blue,
+  child: Text('Something'),
+);
+
+  }
+}
